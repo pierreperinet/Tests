f4ac586e2db1c8cc51d9d1e373bf5948ffa00c39
Pierre Perinet <pierre.perinet@outlook.com>
2018-01-30 12:39:06 -0700

    c17

---
+++ OwinRestService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+
+namespace OwinRestService
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://127.0.0.1:8080/";
+
+            // Start OWIN host 
+            using (WebApp.Start(url: baseAddress))
+            {
+                Console.WriteLine("Service Listening at " + baseAddress);
+
+                System.Threading.Thread.Sleep(-1);
+            }
+        }
+    }
+}
